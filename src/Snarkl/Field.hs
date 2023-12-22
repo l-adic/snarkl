40a6ec3bd77652033fc59ce1869efad60a9f00de
martyall <martyall@protonmail.com>
2023-12-22 12:03:17 -0800

    fix pretty printer

--- src/Snarkl/Field.hs
+++ src/Snarkl/Field.hs
@@ -1,7 +1,13 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Snarkl.Field where
 
-import Data.Field.Galois (Prime)
+import Data.Field.Galois (Prime, PrimeField (fromP))
+import Prettyprinter (Pretty (..))
 
 type P_BN128 = 21888242871839275222246405745257275088548364400416034343698204186575808495617
 
 type F_BN128 = Prime P_BN128
+
+instance Pretty F_BN128 where
+  pretty = pretty . fromP
