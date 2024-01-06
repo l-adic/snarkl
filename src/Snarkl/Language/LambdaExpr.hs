aa40fa4f6647b5bfc7aae096f75122d9e68ba783
martyall <martyall@protonmail.com>
2024-01-06 13:35:31 -0800

    wip

--- src/Snarkl/Language/LambdaExpr.hs
+++ src/Snarkl/Language/LambdaExpr.hs
@@ -4,6 +4,7 @@ module Snarkl.Language.LambdaExpr
   ( Exp (..),
     expOfLambdaExp,
     expBinop,
+    betaNormalize,
   )
 where
 
@@ -33,58 +34,35 @@ deriving instance (Show a) => Show (Exp a)
 
 deriving instance (Eq a) => Eq (Exp a)
 
-type Env a = Map Variable (Exp a)
-
-defaultEnv :: Env a
-defaultEnv = Map.empty
-
-applyLambdas ::
-  Exp a ->
-  (Exp a, Env a)
-applyLambdas expression = runState (go expression) defaultEnv
-  where
-    go :: Exp a -> State (Env a) (Exp a)
-    go = \case
-      EVar var -> do
-        ma <- gets (Map.lookup var)
-        case ma of
-          Nothing -> do
-            pure (EVar var)
-          Just e -> go e
-      e@(EVal _) -> pure e
-      EUnit -> pure EUnit
-      EUnop op e -> EUnop op <$> go e
-      EBinop op es -> EBinop op <$> mapM go es
-      EIf b e1 e2 -> EIf <$> go b <*> go e1 <*> go e2
-      EAssert (EVar v1) e@(EAbs _ _) -> do
-        _e <- go e
-        modify $ Map.insert v1 _e
-        pure EUnit
-      EAssert e1 e2 -> EAssert <$> go e1 <*> go e2
-      ESeq e1 e2 -> ESeq <$> go e1 <*> go e2
-      EAbs var e -> do
-        EAbs var <$> go e
-      EApp (EAbs var e1) e2 -> do
-        _e2 <- go e2
-        modify $ Map.insert var e2
-        go (substitute var e2 e1)
-      EApp e1 e2 -> do
-        _e1 <- go e1
-        _e2 <- go e2
-        go (EApp _e1 _e2)
-
-substitute :: Variable -> Exp a -> Exp a -> Exp a
-substitute var e1 = \case
-  e@(EVar var') -> if var == var' then e1 else e
-  e@(EVal _) -> e
+betaNormalize :: Exp a -> Exp a
+betaNormalize = \case
+  EVar x -> EVar x
+  EVal v -> EVal v
+  EUnop op e -> EUnop op (betaNormalize e)
+  EBinop op es -> EBinop op (betaNormalize <$> es)
+  EIf e1 e2 e3 -> EIf (betaNormalize e1) (betaNormalize e2) (betaNormalize e3)
+  EAssert e1 e2 -> EAssert (betaNormalize e1) (betaNormalize e2)
+  ESeq e1 e2 -> ESeq (betaNormalize e1) (betaNormalize e2)
+  EAbs v e -> EAbs v (betaNormalize e)
+  EApp f a ->
+    case betaNormalize f of
+      EAbs v e -> substitute (v, betaNormalize a) e
+      f' -> EApp f' (betaNormalize a)
   EUnit -> EUnit
-  EUnop op e -> EUnop op (substitute var e1 e)
-  EBinop op es -> EBinop op (map (substitute var e1) es)
-  EIf b e2 e3 -> EIf (substitute var e1 b) (substitute var e1 e2) (substitute var e1 e3)
-  EAssert e2 e3 -> EAssert (substitute var e1 e2) (substitute var e1 e3)
-  ESeq e2 e3 -> ESeq (substitute var e1 e2) (substitute var e1 e3)
-  EAbs var' e -> EAbs var' (substitute var e1 e)
-  EApp e2 e3 -> EApp (substitute var e1 e2) (substitute var e1 e3)
+  where
+    -- substitute x e1 e2 = e2 [x := e1 ]
+    substitute :: (Variable, Exp a) -> Exp a -> Exp a
+    substitute (var, e1) = \case
+      e@(EVar var') -> if var == var' then e1 else e
+      e@(EVal _) -> e
+      EUnit -> EUnit
+      EUnop op e -> EUnop op (substitute (var, e1) e)
+      EBinop op es -> EBinop op (substitute (var, e1) <$> es)
+      EIf b e2 e3 -> EIf (substitute (var, e1) b) (substitute (var, e1) e2) (substitute (var, e1) e3)
+      EAssert e2 e3 -> EAssert (substitute (var, e1) e2) (substitute (var, e1) e3)
+      ESeq e2 e3 -> ESeq (substitute (var, e1) e2) (substitute (var, e1) e3)
+      EAbs var' e -> EAbs var' (substitute (var, e1) e)
+      EApp e2 e3 -> EApp (substitute (var, e1) e2) (substitute (var, e1) e3)
 
 expBinop :: Op -> Exp a -> Exp a -> Exp a
 expBinop op e1 e2 =
@@ -112,7 +90,7 @@ expSeq e1 e2 =
 
 expOfLambdaExp :: (Show a) => Exp a -> Core.Exp a
 expOfLambdaExp _exp =
-  let (coreExp, _) = applyLambdas _exp
+  let coreExp = betaNormalize _exp
    in case expOfLambdaExp' coreExp of
         Left err -> error err
         Right e -> e
